b96c4787bd5443d3203628c295541dd94bc9b33f
gmorelli00 <morelligiovannimg@gmail.com>
2025-06-04 18:49:18 +0200

    little fix

--- src/realloc.c
+++ src/realloc.c
@@ -1,13 +1,9 @@
-#include "malloc.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/mman.h>
-#include <unistd.h>
+#include "../includes/malloc.h"
 
 extern t_heap g_heap;
 
 void *realloc(void *ptr, size_t size)
 {
-    if (ptr == NULL)
-        return(sort_allocations(size));   
+    // if (ptr == NULL)
+    //     return(sort_allocations(size));   
 }
