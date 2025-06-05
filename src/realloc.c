083cc4008e00fb1c78c5a97ffc5f38d37164233a
Leonardo Amici <lamici@Leonardos-MacBook-Air.local>
2025-06-05 17:53:13 +0200

    refactored files

--- src/realloc.c
+++ src/realloc.c
@@ -2,8 +2,16 @@
 
 extern t_heap g_heap;
 
+
+int check_available_alloc(void *ptr, size_t size)
+{
+    t_page *temp;
+    if (size >= aw)
+}
+
 void *realloc(void *ptr, size_t size)
 {
-    // if (ptr == NULL)
-    //     return(sort_allocations(size));   
+    if (ptr == NULL)
+        return(sort_allocations(size));
+    check_available_alloc(ptr, size);
 }
